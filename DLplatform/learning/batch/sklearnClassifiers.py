e88a8970dd1182a1fcf78b2a8eadc6ae48e572d8
Lasal Ranasinghe <lran0009@student.monash.edu>
2020-07-24 20:39:27 +1000

    Started SVM classifier Class

--- DLplatform/learning/batch/sklearnClassifiers.py
+++ DLplatform/learning/batch/sklearnClassifiers.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from typing import List
 # from DLplatform.learning.learner import BatchLearner
@@ -10,48 +9,53 @@ from sklearn.svm import LinearSVC
 
 
 class SupportVectorClassification(BatchLearner):
-    def __init__(self, ):
+    def __init__(self, regParam, random_state, name='SupportVectorClassification'):
+        BatchLearner.__init__(self, name=name)
+        self.regParam = regParam
+        self.model = LinearSVC(C=self.regParam, random_state=random_state)
+
+    def setModel(self, param: VectorParameter, flags: dict):
+
         pass
-
-
-    def setModel(self, param : VectorParameter, flags: dict):
-        pass
-
 
     def train(self, data: List) -> List:
-        pass
 
+        if not isinstance(data, List):
+            error_text = "The argument data is not of type" + str(List) + "it is of type " + str(type(data))
+            self.error(error_text)
+            raise ValueError(error_text)
+        X = np.asarray([record[0] for record in data])
+        y = np.asarray([record[1] for record in data])
 
-    def setParameters(self, param : Parameters):
-        pass
+        score = self.model.fit(X=X, y=y, sample_weight=None).score(X=X, y=y)
+        preds = self.model.predict(X)
+        return score, preds
+
+    def setParameters(self, param: Parameters):
 
 
     def getParameters(self) -> Parameters:
         pass
 
-    
-
-
 
 class LogisticRegression(BatchLearner):
-    def __init__(self, regParam, dim, solver = 'lbfgs', name = "LogisticRegression"):
-        BatchLearner.__init__(self, name = name)
+    def __init__(self, regParam, dim, solver='lbfgs', name="LogisticRegression"):
+        BatchLearner.__init__(self, name=name)
         self.regParam = regParam
         self.dim = dim
         self.solver = solver
         self.model = LR(C=self.regParam, solver=self.solver)
-        self.model.coef_ = np.zeros(dim-1)
+        self.model.coef_ = np.zeros(dim - 1)
         self.model.intercept_ = np.array([0.0])
-        #self.weights = np.zeros(dim)
-        
+        # self.weights = np.zeros(dim)
+
     def setModel(self, param: VectorParameter, setReference: bool):
         super(LogisticRegression, self).setModel(param, setReference)
-        
-        #self.info('STARTTIME_setReference: '+str(time.time()))
+
+        # self.info('STARTTIME_setReference: '+str(time.time()))
         if setReference:
             self._flattenReferenceParams = param.get()
-        #self.info('ENDTIME_setReference: '+str(time.time()))
-
+        # self.info('ENDTIME_setReference: '+str(time.time()))
 
     def train(self, data: List) -> List:
         '''
@@ -74,12 +78,12 @@ class LogisticRegression(BatchLearner):
             raise ValueError(error_text)
         X = np.asarray([record[0] for record in data])
         y = np.asarray([record[1] for record in data])
-        score = self.model.fit(X, y).score(X,y)
+        score = self.model.fit(X, y).score(X, y)
         preds = self.model.predict(X)
-        
+
         return score, preds
 
-    def setParameters(self, param : VectorParameter):
+    def setParameters(self, param: VectorParameter):
         '''
 
         Replace the current values of the model parameters with the values of "param"
@@ -98,17 +102,16 @@ class LogisticRegression(BatchLearner):
         '''
 
         if not isinstance(param, VectorParameter):
-            error_text = "The argument param is not of type" + str(VectorParameter) + "it is of type " + str(type(param))
+            error_text = "The argument param is not of type" + str(VectorParameter) + "it is of type " + str(
+                type(param))
             self.error(error_text)
             raise ValueError(error_text)
-        #TODO: so far, we assume that the intercept is a scalar, but it can be also a 1d-array with len > 1. This would have to be configured somehow...
+        # TODO: so far, we assume that the intercept is a scalar, but it can be also a 1d-array with len > 1. This would have to be configured somehow...
         w = param.get().tolist()
         b = w[-1]
         del w[-1]
         self.model.coef_ = np.array(w)
         self.model.intercept_ = np.array([b])
-
-        
 
     def getParameters(self) -> VectorParameter:
         '''
@@ -121,7 +124,8 @@ class LogisticRegression(BatchLearner):
 
         '''
         wb = np.concatenate((self.model.coef_.flatten(), self.model.intercept_))
-        if isinstance(self.model.intercept_, List): #in principle, the intercept can be a lit. But this may break at other points, then.
+        if isinstance(self.model.intercept_,
+                      List):  # in principle, the intercept can be a lit. But this may break at other points, then.
             wb = np.array(self.model.coef_[0].tolist() + self.model.intercept_.tolist())
         return VectorParameter(wb)
 

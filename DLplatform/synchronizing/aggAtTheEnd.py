b6732a4988e29755396f4935a2397986698b6a85
Lasal Ranasinghe <lran0009@student.monash.edu>
2020-11-16 23:46:45 +1100

    Added rogue evaluate method to test radon point agg

--- DLplatform/synchronizing/aggAtTheEnd.py
+++ DLplatform/synchronizing/aggAtTheEnd.py
@@ -1,12 +1,28 @@
 from DLplatform.synchronizing.synchronizer import Synchronizer
 from DLplatform.parameters import Parameters
 from typing import List
-
+import numpy as np
 
 class AggregationAtTheEnd(Synchronizer):
     def __init__(self, name = "Aggregation-at-the-end"):
         Synchronizer.__init__(self, name = name)
     
+    # def evaluate(self, nodesDict, activeNodes: List[str]) -> (List[str], Parameters):
+    #     if self._aggregator is None:
+    #         self.error("No aggregator is set")
+    #         raise AttributeError("No aggregator is set")
+    #
+    #     # this condition is needed to call the 'evaluate' method in a standardized way across the different sync schemes
+    #     # print('set(list(nodesDict.keys())) == set(activeNodes) evaluates to {}'.format(set(list(nodesDict.keys())) == set(activeNodes)))
+    #     # if len(set(list(nodesDict.keys()))) > 4:
+    #     # print('There are {} nodes in nodesDict.keys(). They are: {}'.format(len(set(list(nodesDict.keys()))), set(list(nodesDict.keys()))))
+    #     # print('There are {} active nodes. They are: {}'.format(len(set(activeNodes)), set(activeNodes)))
+    #     if set(list(nodesDict.keys())) == set(activeNodes):
+    #         print('About to return activeNodes, self._aggregator(list(nodesDict.values())), {}')
+    #         return activeNodes, self._aggregator(list(nodesDict.values())), {}
+    #     else:
+    #         return [], None, {}
+
     def evaluate(self, nodesDict, activeNodes: List[str]) -> (List[str], Parameters):
         if self._aggregator is None:
             self.error("No aggregator is set")
@@ -18,8 +34,16 @@ class AggregationAtTheEnd(Synchronizer):
         # print('There are {} nodes in nodesDict.keys(). They are: {}'.format(len(set(list(nodesDict.keys()))), set(list(nodesDict.keys()))))
         # print('There are {} active nodes. They are: {}'.format(len(set(activeNodes)), set(activeNodes)))
         if set(list(nodesDict.keys())) == set(activeNodes):
+            node_dict_list = list(nodesDict.values())
+            print('nodesDict.values():', nodesDict.values())
+            node_vec = node_dict_list[0].get()
+            print('node_vec:', node_vec)
+            node_vec = node_vec + np.array([-0.4, 0.3, -0.5])
+            print('changed node_vec:', node_vec)
+            node_dict_list[0].set(weights=node_vec)
+            print('node_dict_list[0].get():', node_dict_list[0].get())
             print('About to return activeNodes, self._aggregator(list(nodesDict.values())), {}')
-            return activeNodes, self._aggregator(list(nodesDict.values())), {}
+            return activeNodes, self._aggregator(node_dict_list), {}
         else:
             return [], None, {}
 
